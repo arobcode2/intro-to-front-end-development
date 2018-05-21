c6ed0fceb3bd5e174bc310419785a88f9810aec3
Andrew Rob <andrew-c-rob@live.com>
2018-05-21 18:06:15 -0500

    Implemented HTML, CSS, jQuery Part One

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 /* YOUR CODE HERE */
+$(document).ready(function() {
+	$('.btn').on('click', function() {
+		var randomColors = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+		$('.square').css('background-color', randomColors);
+	});
+});
+

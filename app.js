e5e150ed92c039ba27e369d1e576fc7bab59499e
Andrew Rob <andrew-c-rob@live.com>
2018-05-21 19:07:24 -0500

    Working on HTML, CSS, jQuery Part Two

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 /* YOUR CODE HERE */
 $(document).ready(function() {
+	var sqr = $('.square');
+	var newSqr = '<div class="square" style="width: 100px;height: 100px;float:right;"></div>';
 	$('.btn').on('click', function() {
 		var randomColors = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-		$('.square').css('background-color', randomColors);
+		sqr.css('background-color', randomColors);
+	});
+	$('.btn2').on('click', function() {
+		var randomColors = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+		$(sqr).after($(newSqr).css('background-color', randomColors));
 	});
 });
 
